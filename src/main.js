b09faa3b3055e1eea0e39b32a57ea775db090e08
Riky-Droyg <riky.droyg@gmail.com>
2025-12-13 08:45:34 +0200

    added basic structure und styles

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import "/js/header.js";
+import "/js/hero.js";
+import "/js/pets-list.js";
+import "/js/about-us.js";
+import "/js/faq.js";
+import "/js/success-stories.js";
+import "/js/animal-details.js";
+import "/js/order-modal.js";
+import "/js/footer.js";
+
+
+
