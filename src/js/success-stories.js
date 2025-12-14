d09f7ecf11b0db5db66b94519b7559454925d369
Oleksii Ponomarenko <alexponomarenko996@gmail.com>
2025-12-14 20:22:51 +0100

    update code

---
+++ src/js/success-stories.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import swiper from 'swiper';
+
+/* #region  global variables */
+let ITEMS_PER_PAGE = 0;
+
+axios.defaults.baseURL = 'https://paw-hut.b.goit.study';
+export const API_ENDPOINTS = {
+  FEEDBACKS: '/api/feedbacks',
+};
+
+const BREAKPOINTS = {
+  desktop: 1440,
+  tablet: 768,
+  mobile: 375,
+};
+/* #endregion */
